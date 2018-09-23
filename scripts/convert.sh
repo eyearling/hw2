f3c620437796dd195ecbf57cfe0bc77490c597d2
Emily Yearling <emilyyearling@Emilys-MacBook-Pro.local>
2018-09-23 16:20:57 -0400

    changes to scripts/convert.sh

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 ### INSERT YOUR CODE BELOW ###
+#create example .json files
+#dcm2bids_helper -d raw/IBRAIN002 -o bids
 
+#convert dicom files
+dcm2bids -d raw/IBRAIN002 -p $1 -c scripts/dcm2bids.json -o bids/data
 
 ### DO NOT MODIFY THE LINES BELOW ###
 uname -a > info_host.txt
